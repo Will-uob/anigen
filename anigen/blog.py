941bb10f160af36f7f3a12fa56c08ce82e9b65e0
Will-uob <wxk156@student.bham.ac.uk>
2023-10-21 19:49:34 +0100

    Is this what I'm looking for?

--- anigen/blog.py
+++ anigen/blog.py
@@ -12,7 +12,7 @@ import requests
 import io
 
 API_URL = "https://api-inference.huggingface.co/models/hakurei/waifu-diffusion"
-API_TOKEN = ""
+API_TOKEN = "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"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
 from uuid import uuid4
